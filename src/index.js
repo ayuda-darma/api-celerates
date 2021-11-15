8526900c45d01140ad1925d5999624a38831fb5f
Ayuda Darmawan Yulianto <ayuda.darmawan@gmail.com>
2021-11-15 20:46:25 +0700

    port

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const swaggerSpec = {
 
 // settings
 const app = express();
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 // middlewares
 app.use(express.json());
